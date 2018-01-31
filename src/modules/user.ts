6a32ab15f18ec3a7a23d1b2ad4555df21473458f
Fozilbek <fozilbek.samiyev@gmail.com>
2018-01-31 13:54:01 +0500

    feature

--- src/modules/user.ts
+++ src/modules/user.ts
@@ -1,11 +1,11 @@
-import {Define, ISanction, IMethod} from "@samiyev/json-rpc-2.0";
+import {Define, IFeature, IMethod} from "@samiyev/json-rpc-2.0";
 
 export class User extends Define {
     public static modulename: string = 'user';
-    public static sanction: ISanction;
+    public static feature: IFeature;
 
     public static authorization: IMethod = {
-        sanction: {
+        feature: {
             session: 'active'
         },
         operation: async function (params): Promise<any> {
@@ -13,20 +13,20 @@ export class User extends Define {
         }
     };
 
-    public static registration = {
-        sanction: {
+    public static registration: IMethod = {
+        feature: {
             session: 'active'
         },
-        action: async function (params): Promise<any> {
+        operation: async function (params): Promise<any> {
 
         }
     };
 
-    public static get_info = {
-        sanction: {
+    public static get_info: IMethod = {
+        feature: {
             session: 'active'
         },
-        action: async function (params): Promise<any> {
+        operation: async function (params): Promise<any> {
 
         }
     }
